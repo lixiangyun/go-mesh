3ad9d353417ce6aee3d06b59de7d39dd694aa5ea
linimbus <linimbus@126.com>
2018-07-07 22:18:08 +0800

    fix bug

--- mesher/log/log.go
+++ mesher/log/log.go
@@ -1,41 +1,44 @@
 package log
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
 
 var logfile *os.File
 
+func openlogfile(name string) error {
+
+	file, err := os.OpenFile(name, os.O_WRONLY, 0)
+	if err != nil {
+		file, err = os.Create(name)
+		if err != nil {
+			fmt.Println("create file error!", err.Error())
+			return err
+		}
+		fmt.Println("create log file!", name)
+	} else {
+		fileinfo, err := file.Stat()
+		if err == nil {
+			file.Seek(fileinfo.Size(), 0)
+			fmt.Println("append log to file ", file.Name())
+		}
+	}
+	logfile = file
+
+	return nil
+}
+
 func SetLogFile(name string) error {
 
-	var err error
-
-	logfile, err = os.Open(name)
-	if err == nil {
-
-		fstat, err := logfile.Stat()
-		if err != nil {
-			log.Println(err.Error())
-			return err
-		}
-
-		logfile.Seek(fstat.Size(), 0)
-
-		log.SetOutput(logfile)
-		return nil
+	err := openlogfile(name)
+	if err != nil {
+		log.Println(err.Error())
+		return err
 	}
 
-	if os.IsNotExist(err) {
-		logfile, err := os.Create(name)
-		if err != nil {
-			log.Println(err.Error())
-			return err
-		}
-		log.SetOutput(logfile)
-		return nil
-	}
-	log.Println(err.Error())
+	log.SetOutput(logfile)
 
-	return err
+	return nil
 }
