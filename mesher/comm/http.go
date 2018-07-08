ec747e678a2324f233ee760d61c97d4f51dec494
linimbus <linimbus@126.com>
2018-07-08 13:00:23 +0800

    update

--- mesher/comm/http.go
+++ mesher/comm/http.go
@@ -7,42 +7,47 @@ import (
 	"time"
 )
 
-var defaultTransport http.RoundTripper = &http.Transport{
-	DialContext: (&net.Dialer{
-		Timeout:   10 * time.Second,
-		KeepAlive: 30 * time.Second,
-		DualStack: true,
-	}).DialContext,
-	MaxIdleConnsPerHost:   10,
-	MaxIdleConns:          10,
-	IdleConnTimeout:       30 * time.Second,
-	TLSHandshakeTimeout:   30 * time.Second,
-	ExpectContinueTimeout: 1 * time.Second,
+type httpClientMap struct {
+	sync.Mutex
+	clients map[string]*http.Client
 }
 
-var gLock sync.Mutex
-
-var gHttpClientMap map[string]*http.Client
+var gHttpClientAll httpClientMap
 
 func init() {
-	gHttpClientMap = make(map[string]*http.Client, 100)
+	gHttpClientAll.clients = make(map[string]*http.Client, 100)
+}
+
+func newTransport() http.RoundTripper {
+	return &http.Transport{
+		DialContext: (&net.Dialer{
+			Timeout:   10 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConnsPerHost:   10,
+		MaxIdleConns:          10,
+		IdleConnTimeout:       30 * time.Second,
+		TLSHandshakeTimeout:   30 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
 }
 
 func newhttpclient() *http.Client {
 	return &http.Client{
-		Transport: defaultTransport,
+		Transport: newTransport(),
 		Timeout:   10 * time.Second,
 	}
 }
 
 func HttpClient(addr string) *http.Client {
-	gLock.Lock()
-	client, b := gHttpClientMap[addr]
+	gHttpClientAll.Lock()
+	client, b := gHttpClientAll.clients[addr]
 	if b == false {
 		client = newhttpclient()
-		gHttpClientMap[addr] = client
+		gHttpClientAll.clients[addr] = client
 	}
-	defer gLock.Unlock()
+	gHttpClientAll.Unlock()
 
 	return client
 }
